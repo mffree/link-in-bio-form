3e4a49f9cdcd0944e6c4cb2ec900d4f089f59699
mffree <matthewfreeman63@gmail.com>
2025-02-20 14:04:44 +0000

    added insert item and redirect so all done

--- app/controllers/bd_controller.rb
+++ app/controllers/bd_controller.rb
@@ -2,4 +2,22 @@ class BdController < ApplicationController
   def backdoor
     render({ :template => "backdoor_templates/backdoor_page" })
   end
+
+  def insert
+    # Fetching the form inputs
+    link_url_fetched = params.fetch("query_link_url")
+    link_description_fetched = params.fetch("query_link_description")
+    thumbnail_url_fetched = params.fetch("query_thumbnail_url")
+
+    # Creating the database entry
+    x = Item.new
+    x.link_url = link_url_fetched
+    x.link_description = link_description_fetched
+    x.thumbnail_url = thumbnail_url_fetched
+    x.save
+
+    redirect_to("/")
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
 
-  get("/backdoor", { :controller => "bd", :action => "backdoor" })
+  get("/backdoor", { :controller => "bd", :action => "backdoor" }) # backdoor page
+
+  post("/insert_item", { :controller => "bd", :action => "insert" }) # insert item POST route
+
 
 end
